763997500da8792d779009b9db539ab5db95aff8
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-18 21:27:22 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,12 @@
 def my_select(collection)
  # your code here!
  i=0
+ newArr=Array.new
   while i<collection.length
     if yield(collection[i]) == true
-      return collection[i]
+      newArr << collection[i]
     end
     i+=1
   end
+  newArr
 end
